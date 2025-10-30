b222cc84e50911db5b1db36200252be90112615c
Julián Esteban Gáez Vargas <vargasjulian.2207@gmail.com>
2025-10-29 21:01:32 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Portafolio.github.io',
   integrations: [
     tailwind(),
     mdx(),
